9ee9b8d1c9fb51498aff00d3f323e5d6e3584893
benym <benyuanming@gmail.com>
2021-12-13 22:59:17 +0800

    [feature]XA模式

--- seata-demo/order-service/src/main/java/cn/itcast/order/service/impl/OrderServiceImpl.java
+++ seata-demo/order-service/src/main/java/cn/itcast/order/service/impl/OrderServiceImpl.java
@@ -6,6 +6,7 @@ import cn.itcast.order.entity.Order;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.service.OrderService;
 import feign.FeignException;
+import io.seata.spring.annotation.GlobalTransactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,7 +27,7 @@ public class OrderServiceImpl implements OrderService {
     }
 
     @Override
-    @Transactional
+    @GlobalTransactional
     public Long create(Order order) {
         // 创建订单
         orderMapper.insert(order);
