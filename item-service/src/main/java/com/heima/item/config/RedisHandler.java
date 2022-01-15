f842659b689986f0c301af99e4eee6b2cc8efac4
benym <benyuanming@gmail.com>
2022-01-15 18:04:43 +0800

    [feature]Redis配置

---
+++ item-service/src/main/java/com/heima/item/config/RedisHandler.java
@@ -0,0 +1,58 @@
+package com.heima.item.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.heima.item.pojo.Item;
+import com.heima.item.pojo.ItemStock;
+import com.heima.item.service.IItemService;
+import com.heima.item.service.IItemStockService;
+import com.heima.item.service.impl.ItemService;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @Time : 2022/1/15 17:49
+ * @Author : YuanMing
+ * @File : RedisHandler.java
+ * @Software: IntelliJ IDEA
+ */
+@Component
+public class RedisHandler implements InitializingBean {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Autowired
+    private IItemService itemService;
+
+    @Autowired
+    private IItemStockService stockService;
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 初始化缓存，缓存预热
+        // 1. 查询商品信息
+        List<Item> itemList = itemService.list();
+        // 2. 放入缓存
+        for (Item item : itemList) {
+            // 2.1 item序列化为json
+            String itemJson = MAPPER.writeValueAsString(item);
+            // 2.2 存入redis
+            redisTemplate.opsForValue().set("item:id:" + item.getId(), itemJson);
+        }
+        // 3. 查询商品库存信息
+        List<ItemStock> stockList = stockService.list();
+        // 4. 放入缓存
+        for (ItemStock stock : stockList) {
+            // 4.1 stock序列化为json
+            String stockJson = MAPPER.writeValueAsString(stock);
+            // 4.2 存入redis
+            redisTemplate.opsForValue().set("stock:id:" + stock.getId(), stockJson);
+        }
+    }
+}
